99edce21a1f6788958bccbfd2e639079e099c4a3
Yusuf-ASM <y.alsayed@nu.edu.eg>
2022-12-19 12:53:21 +0200

    done with getroom

--- rlServer/roomLocator.js
+++ rlServer/roomLocator.js
@@ -1,21 +1,34 @@
 const express = require("express");
+
+const backend = require("./backend.js");
+
 const app = express();
 app.use(express.json());
+
 const router = express.Router();
 const port = 8080;
 
 router.get("/api", (req, res) => {
   res.send({ NuCoders: "Hello World" });
 });
-router.get("/api/getroom/", (req, res) => {
+
+// ok
+router.get("/api/getroom/", async (req, res) => {
   // 127.0.0.1:8080/api/getroom/
   let id = req.body["id"];
   if (id == null || id == "") {
     res.status(404).json({ error: "Not a room" });
   } else {
-    res.send({ getroom: id });
+    let to_return  = await backend.getRoom(id);
+    if (to_return === 0){
+      res.status(404).json({ error: "Not a room" });
+    }else{
+      console.log(to_return);
+      res.send({ "getroom": to_return });
+    }
   }
 });
+
 router.get("/api/getrooms/", (req, res) => {
   // http://127.0.0.1:8080/api/getrooms/
   let building = req.body["building"];
@@ -25,7 +38,9 @@ router.get("/api/getrooms/", (req, res) => {
     res.send({ "getrooms": building });
   }
 });
+
 router.get("/api/roomtable", (req, res) => {
+  // http://127.0.0.1:8080/api/roomtable/
   let id = req.body["id"];
   if (id == null || id == "") {
     res.status(404).json({ error: "Not a room" });
@@ -33,12 +48,14 @@ router.get("/api/roomtable", (req, res) => {
     res.send({ "roomtable": id });
   }
 });
-router.get("/api/whatsin", (req, res) => {
+
+router.get("/api/whatsin", async (req, res) => {
+  // http://127.0.0.1:8080/api/whatsin/
   let id = req.body["id"];
   if (id == null || id == "") {
-    res.status(404).json({ error: "Not a room" });
+    await res.status(404).json({ error: "Not a room" });
   } else {
-    res.send({ "whatsin": id });
+    await res.send({ "whatsin": id });
   }
 });
 
