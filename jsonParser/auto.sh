064770816151991719a6a1d7dd1c507875e6cf83
Yusuf-ASM <y.alsayed@nu.edu.eg>
2022-12-19 11:56:34 +0200

    v2 of parser

--- jsonParser/auto.sh
+++
@@ -1,5 +0,0 @@
-git add .
-git commit -m "Renaming parser.js to jsonExport.js, adding readme file, fix unicode error in json files and organizing file structure"
-git push origin rl
-# git push gitlab master
-# bash auto.sh
--- jsonParser/test.js
+++
@@ -1,4 +0,0 @@
-let a = "01A"
-let len = a.length-1
-console.log(a.charAt(len));
-console.log(a.substring(0,len));
