61d9f5ad60c07590df0780bccccd58d2488ccca3
Yusuf-ASM <y.alsayed@nu.edu.eg>
2022-12-19 11:40:19 +0200

    done with rooms.json

--- jsonParser/playGround.py
+++ jsonParser/playGround.py
@@ -1,44 +1,94 @@
-# slots = {
-#     80: 1,
-#     830: 2,
-#     90: 3,
-#     930: 4,
-#     100: 5,
-#     1030: 6,
-#     110: 7,
-#     1130: 8,
-#     120: 9,
-#     1230: 10,
-#     130: 11,
-#     1330: 12,
-#     140: 13,
-#     1430: 14,
-#     150: 15,
-#     1530: 16,
-#     160: 17,
-#     1630: 18,
-#     170: 19,
-#     1730: 20,
-#     180: 21,
-#     1830: 22,
-#     190: 23,
-#     1930: 24,
-#     200: 25,
-#     2030: 26
-# }
-
 
 
 import json
-courses_json = json.load(open("./json/courses.json","r"))
+courses_json = json.load(open(r"./json/courses.json", "r+"))
 rooms = dict()
+
+
 def room_creator(courses):
+    output = open("output.json", "w")
+    test_file = open(r"./template.json", "r+")
+    test_json = json.load(test_file)
+    test = test_json["test"]
     for course in courses:
-        print(course["schedules"])
+        for schedule in course["schedules"]:
+            room = schedule["roomId"]
+            if room not in ["Online", ""]:
+                if room not in test_json:
+                    test_json[room] = test
+                    test_json[room]["building"] = schedule["bldgName"][-1]
+    output.write(json.dumps(test_json))
+
 
 room_creator(courses=courses_json)
-start_sloth = 8
-end_sloth = 10
-start_slotm = 0
-end_slotm = round(29,-1)
-print((end_sloth - start_sloth) * 2 + abs(end_slotm - start_slotm)//30)
+
+
+slots = {
+    '830': 1,
+    '9': 2,
+    '930': 3,
+    '10': 4,
+    '1030': 5,
+    '11': 6,
+    '1130': 7,
+    '12': 8,
+    '1230': 9,
+    '13': 10,
+    '1330': 11,
+    '14': 12,
+    '1430': 13,
+    '15': 14,
+    '1530': 15,
+    '16': 16,
+    '1630': 17,
+    '17': 18,
+    '1730': 19,
+    '18': 20,
+    '1830': 21,
+    '19': 22,
+    '1930': 23,
+    '20': 24,
+    '2030' : 25
+}
+
+# TODO CREATE FUNCTON TO GET SLOT
+
+def calculate_slots(slot):
+    hour = slot[0]
+    minute = slot[1]
+    if minute == 0:
+        return slots[f"{hour}"]
+    elif minute <= 30:
+        return slots[f"{hour}30"]
+    elif minute > 30:
+        return slots[f"{hour+1}"]
+
+
+s = calculate_slots([9, 0, 0])
+e = calculate_slots([11, 59, 0])
+
+# TODO DO LOOP BY RANGE(START , END)
+for i in range(s, e):
+    print(i)
+
+
+def slots_filler():
+    output = json.load(open("output.json", "r"))
+    output2 = open("output2.json", "w")
+
+    for course in courses_json:
+        id = course['id']
+        for schedule in course["schedules"]:
+            room = schedule["roomId"]
+            if room not in ["Online", ""]:
+                room_id = schedule['roomId']
+                day = schedule['scheduledDays'][0]
+                start_slot = calculate_slots(schedule['scheduledStartTime'])
+                end_slot = calculate_slots(schedule['scheduledEndTime'])
+                for slot in range(start_slot,end_slot):
+                    output[room_id][f"{day}"][f"{slot}"]['status'] = False
+                    output[room_id][f"{day}"][f"{slot}"]['id'] = id
+    output2.write(json.dumps(output))
+
+     
+slots_filler()
