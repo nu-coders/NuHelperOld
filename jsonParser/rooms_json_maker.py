007f4dc3d318f534d1b08d970060a3a8b38f90cc
Yusuf Sabry <alexhue4090@gmail.com>
2022-12-23 11:40:05 +0200

    fix wrong building and floor info in rooms.json

--- jsonParser/rooms_json_maker.py
+++ jsonParser/rooms_json_maker.py
@@ -1,27 +1,6 @@
-
-
 import json
 courses_json = json.load(open(r"./json/courses.json", "r+"))
-rooms = dict()
-
-
-def room_creator(courses):
-    output = open("output.json", "w")
-    test_file = open(r"./template.json", "r+")
-    test_json = json.load(test_file)
-    test = test_json["test"]
-    for course in courses:
-        for schedule in course["schedules"]:
-            room = schedule["roomId"]
-            if room not in ["Online", ""]:
-                if room not in test_json:
-                    test_json[room] = test
-                    test_json[room]["building"] = schedule["bldgName"][-1]
-    output.write(json.dumps(test_json))
-
-
-room_creator(courses=courses_json)
-
+rooms_json = open(r"./json/rooms.json", "w+")
 
 slots = {
     '830': 1,
@@ -48,8 +27,38 @@ slots = {
     '19': 22,
     '1930': 23,
     '20': 24,
-    '2030' : 25
+    '2030': 25
 }
+
+floors = {
+    "BUB1": "Basement floor of Tarek Khalil Building - BUB1",
+    "GUB1": "Ground floor of Tarek Khalil Building - GUB1",
+    "FUB1": "First floor of Tarek Khalil Building - FUB1",
+    "F1": "First floor of Tarek Khalil Building - F1",
+    "SUB1": "Second floor of Tarek Khalil Building - SUB1",
+    "BUB2": "Basement floor of Second -Main- Building - BUB2",
+    "GUB2": "Ground floor of Second -Main- Building - GUB2",
+    "FUB2": "First floor of Second -Main- Building - FUB2",
+    "F": "First floor of Second -Main- Building - FUB2",
+    "SUB2": "Second floor of Second -Main- Building - SUB2",
+    "": "N/A"
+}
+
+
+def room_creator(courses):
+    template_json = json.load(open(r"./template.json", "r"))
+    template = template_json["test"]
+    template_json.pop("test")
+    for course in courses:
+        for schedule in course["schedules"]:
+            room = schedule["roomId"]
+            if room not in ["Online", ""]:
+                if room not in template_json:
+                    template_json[room] = template.copy()
+                    template_json[room]["building"] = schedule["bldgName"][-1]
+                    template_json[room]["floor"] = floors[schedule["floorId"]]
+    return template_json
+
 
 # TODO CREATE FUNCTON TO GET SLOT
 
@@ -64,20 +73,8 @@ def calculate_slots(slot):
         return slots[f"{hour+1}"]
 
 
-s = calculate_slots([9, 0, 0])
-e = calculate_slots([11, 59, 0])
-
-# TODO DO LOOP BY RANGE(START , END)
-for i in range(s, e):
-    print(i)
-
-
-def slots_filler():
-    output = json.load(open("output.json", "r"))
-    output2 = open("output2.json", "w")
-
+def slots_filler(input_json):
     for course in courses_json:
-        id = course['id']
         for schedule in course["schedules"]:
             room = schedule["roomId"]
             if room not in ["Online", ""]:
@@ -85,10 +82,35 @@ def slots_filler():
                 day = schedule['scheduledDays'][0]
                 start_slot = calculate_slots(schedule['scheduledStartTime'])
                 end_slot = calculate_slots(schedule['scheduledEndTime'])
-                for slot in range(start_slot,end_slot):
-                    output[room_id][f"{day}"][f"{slot}"]['status'] = False
-                    output[room_id][f"{day}"][f"{slot}"]['id'] = id
-    output2.write(json.dumps(output))
+                for slot in range(start_slot, end_slot):
+                    input_json[room_id][f"{day}"][f"{slot}"]['status'] = False
+                    # input_json[room_id][f"{day}"][f"{slot}"]['course'] =  course["eventId"]
+                    # input_json[room_id][f"{day}"][f"{slot}"]['type'] = course["eventSubType"]
+                    # input_json[room_id][f"{day}"][f"{slot}"]['section'] = course["section"]
+    return input_json
 
-     
-slots_filler()
+
+def empty_vacant(input_json):
+    days = ["0", "1", "2", "3", "4", "6"]
+    slots = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"]
+    slots_len = len(slots)
+    for room in input_json:
+        for day in days:
+            for slot in range(slots_len):
+                status = input_json[room][day][slots[slot]]["status"]
+                temp_status = status
+                for i in range(slot,slots_len):
+                    if status != temp_status:
+                        input_json[room][day][slots[slot]]["E/V"] = i
+                        break
+                    temp_status = input_json[room][day][slots[i]]["status"]
+    return input_json
+
+
+rooms = room_creator(courses_json)
+filled = slots_filler(rooms)
+
+# inp = open("./json/rooms.json", "r")
+# inpo = open("./json/rooms2.json", "w")
+# rooms_json.write(json.dumps(empty_vacant(filled)))
+rooms_json.write(json.dumps(filled))
---
+++ jsonParser/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+buildings = {
+    "BUB1": "Basement floor of Tarek Khalil Building - BUB1",
+    "GUB1": "Ground floor of Tarek Khalil Building - GUB1",
+    "FUB1": "First floor of Tarek Khalil Building - FUB1",
+    "F1": "First floor of Tarek Khalil Building - F1",
+    "SUB1": "Second floor of Tarek Khalil Building - SUB1",
+    "BUB2": "Basement floor of Second -Main- Building - BUB2",
+    "GUB2": "Ground floor of Second -Main- Building - GUB2",
+    "FUB2": "First floor of Second -Main- Building - FUB2",
+    "F": "First floor of Second -Main- Building - FUB2",
+    "SUB2": "Second floor of Second -Main- Building - SUB2",
+    "": "N/A"
+}
+print(buildings["FUB1"])
